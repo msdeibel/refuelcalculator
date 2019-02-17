20abc931c1124bc5e4932459394a5753ae2e8057
msdeibel <git2018@markus-deibel.de>
2019-02-17 09:12:24 +0100

    Accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ConsumptionComponent } from './consumption/consumption.component';
@@ -14,7 +15,8 @@ import { StationComponent } from './station/station.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
