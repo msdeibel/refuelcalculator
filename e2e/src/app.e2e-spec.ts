f9b8de7cd844cbe42a2bddc42324bd72ac2f617e
msdeibel <git2018@markus-deibel.de>
2019-03-05 20:28:10 +0100

    Prod build for github.io

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to tankrechner!');
+    expect(page.getTitleText()).toEqual('Welcome to Refuel Calculator!');
   });
 
   afterEach(async () => {
